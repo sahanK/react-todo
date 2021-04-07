c683d122fad9e9ac1ce850d1d38808748b1c4020
Sahan Kaushalya <sahankaushalya01@gmail.com>
2021-04-07 12:52:30 +0530

    bug fixed Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -24,7 +24,7 @@ const Form = ({inputText, setInputText, todos, setTodos, setStatus}) => {
                 <i className="fas fa-plus-square"></i>
             </button>
             <div onChange={statusHandler} className="select">
-                <select name="todos" clsassName="filter-todo">
+                <select name="todos" className="filter-todo">
                     <option value="all">All</option>
                     <option value="completed">Completed</option>
                     <option value="uncompleted">Uncompleted</option>
