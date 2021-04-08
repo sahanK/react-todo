57c4e2ea76c830f409ba1edc0e0b289f5a14318c
Sahan Kaushalya <sahankaushalya01@gmail.com>
2021-04-08 17:06:08 +0530

    bugs fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import {useState, useEffect} from 'react';
-import Footer from './components/Footer';
-import Home from './components/Home';
 import {useRoutes} from 'react-router-dom';
 import routes from './routes'
 
 function App() {
-  const routing = useRoutes(routes);
 
   return (
     (useRoutes(routes))
